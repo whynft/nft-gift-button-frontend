cdd439ac402043acb87c7fa69d4ae6e68329e471
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2021-12-12 22:33:41 +0800

    add logic with verification code

--- src/web/urls.py
+++ src/web/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('send', TemplateView.as_view(
         template_name="web/send-gift.html",
         extra_context={
-            'BACKEND_DARILKA_API': os.getenv("BACKEND_DARILKA_API")},
+            'BACKEND_DARILKA_API': os.getenv("BACKEND_DARILKA_API"),
+            'BACKEND_VERIFICATION_CODE_API': os.getenv("BACKEND_VERIFICATION_CODE_API")},
     )),
     path('receive', TemplateView.as_view(
         template_name="web/receive-gift.html",
