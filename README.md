b340ae0d87a290f7c7b270cd207617c281f69f64
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2021-12-12 18:11:04 +0800

    add html simple example

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nft-gift-button-frontend
+
+Currently, there is a Django as html/static server only. 
+Thus, no interaction with the Django backend as it often happens.
+To note, all frontend logic is **inside client html** file 
+and itself it consists of **pure frontend  html** that will migrate to reactApp in the future, i.e. in the second milestone accroding to grant roadmap. 
+
+# Start
+Prepare `.env` according to [.env.example](.env.example) and
+```
+docker-compose up
+```
---
+++ src/web/urls.py
@@ -0,0 +1,20 @@
+import os
+
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'web'
+
+urlpatterns = [
+    path('send', TemplateView.as_view(
+        template_name="web/send-gift.html",
+        extra_context={
+            'BACKEND_DARILKA_API': os.getenv("BACKEND_DARILKA_API")},
+    )),
+    path('receive', TemplateView.as_view(
+        template_name="web/receive-gift.html",
+        extra_context={
+            'BACKEND_BOOK_API': os.getenv("BACKEND_BOOK_API"),
+            'BACKEND_DARILKA_API': os.getenv("BACKEND_DARILKA_API")},
+    )),
+]
